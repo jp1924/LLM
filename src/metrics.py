b89cb109cbad39ed5829cb67a148b8ef3e2be94f
jp1924 <jsb10121249@gmail.com>
2025-05-23 03:40:08 +0000

    Update: compute_tnt_metrics 함수에서 WER 및 CER 계산 로직 개선 및 wandb 테이블 추가

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,9 @@
 import numpy as np
 import wandb
+from accelerate import Accelerator
 from evaluate import load
+from torch.utils.data import DataLoader, DistributedSampler
+from tqdm import tqdm
 
 from transformers import EvalPrediction, PreTrainedTokenizer, TrainingArguments
 
@@ -11,31 +14,52 @@ def compute_tnt_metrics(
     args: TrainingArguments,
 ) -> dict[str, float]:
     eval_pred.predictions[eval_pred.predictions == -100] = tokenizer.pad_token_id
-    preds, labels = (
-        [
-            x.replace(tokenizer.pad_token, "").split("<|im_start|>")[-1].replace("<|im_end|>", "")
-            for x in tokenizer.batch_decode(eval_pred.predictions, skip_special_tokens=False)
-        ],
-        [
-            x.replace(tokenizer.pad_token, "")
-            for x in tokenizer.batch_decode(eval_pred.label_ids, skip_special_tokens=False)
-        ],
-    )
+    predictions = tokenizer.batch_decode(eval_pred.predictions, skip_special_tokens=False)
+    src = [x[0].replace(tokenizer.pad_token, "").split("<|im_start|>")[-1].split("<|im_end|>")[0] for x in predictions]
 
-    wer_score_ls, cer_score_ls = [], []
-    predictions_table = wandb.Table(columns=["Prediction", "Label", "WER", "CER"])
+    eval_pred.label_ids[eval_pred.label_ids == -100] = tokenizer.pad_token_id
+    label_ids = tokenizer.batch_decode(eval_pred.label_ids, skip_special_tokens=False)
+    lbl = [x[0].replace(tokenizer.pad_token, "") for x in label_ids]
+
+    wer_score_ls, cer_score_ls, result_ls = [], [], []
     wer, cer = load("evaluate-metric/wer"), load("evaluate-metric/cer")
 
-    for pred, label in zip(preds, labels):
-        wer_scores = wer.compute(predictions=preds, references=labels)
-        cer_scores = cer.compute(predictions=preds, references=labels)
-        wer_score_ls.append(wer_scores)
-        cer_score_ls.append(cer_scores)
-        predictions_table.add_data(pred, label, wer_scores, cer_scores)
+    if "wandb" in args.report_to:
+        predictions_table = wandb.Table(columns=["Prediction", "Label", "WER", "CER"])
+
+        sample_ls = list(zip(lbl, src))
+        data_loader = DataLoader(
+            sample_ls,
+            batch_size=1,
+            sampler=DistributedSampler(
+                sample_ls,
+                num_replicas=args.world_size,
+                rank=args.local_process_index,
+                shuffle=False,
+            ),
+        )
+        accelerator = Accelerator()
+        for pred, label in tqdm(data_loader, position=args.local_process_index):
+            wer_scores = wer.compute(predictions=pred, references=label)
+            cer_scores = cer.compute(predictions=pred, references=label)
+            wer_score_ls.append(wer_scores)
+            cer_score_ls.append(cer_scores)
+            result_ls.append((pred, label, wer_scores, cer_scores))
+
+        if args.parallel_mode != "not_distributed":
+            wer_score_ls = accelerator.gather_for_metrics(wer_score_ls)
+            cer_score_ls = accelerator.gather_for_metrics(cer_score_ls)
+            result_ls = accelerator.gather_for_metrics(result_ls)
+
+        for pred, label, wer_scores, cer_scores in result_ls:
+            predictions_table.add_data(pred, label, wer_scores, cer_scores)
+    else:
+        wer_scores = wer.compute(predictions=src, references=lbl)
+        cer_scores = cer.compute(predictions=src, references=lbl)
 
     return {
-        "wer": np.mean(wer_score_ls),
-        "cer": np.mean(cer_score_ls),
+        "wer": np.mean(wer_score_ls) if wer_score_ls else wer_scores,
+        "cer": np.mean(cer_score_ls) if cer_score_ls else cer_scores,
         "predictions_result": predictions_table,
     }
 
